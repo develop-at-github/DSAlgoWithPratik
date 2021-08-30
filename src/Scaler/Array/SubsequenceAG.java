f97aac7b1d263c92c9bd6645feb0e10404becc72
Pratik_Desai <pratik.desai@volantetech.com>
2021-08-30 16:28:46 +0530

    Scaler array Problems added

--- src/Scaler/Array/SubsequenceAG.java
+++ src/Scaler/Array/SubsequenceAG.java
@@ -1,7 +1,27 @@
 package Scaler.Array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubsequenceAG {
     public static void main(String[] args) {
+        String str="ABCGAG";
+        System.out.println(getSubsequence_Count(str));
+    }
 
+    public static int getSubsequence_Count(String str){
+        int count=0;
+        int noOf_G_TillNow=0;
+        char[] charr=str.toCharArray();
+
+        for (int i=charr.length-1;i>=0;i--){
+            if (charr[i]=='G'){
+                noOf_G_TillNow++;
+            }
+            else if (charr[i]=='A'){
+                count+=noOf_G_TillNow;
+            }
+        }
+        return count;
     }
 }
