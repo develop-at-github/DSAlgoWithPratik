5060701cb58e81bb9d75ecdd86300c5b5a55484c
Aditya Thakur <aditya.t.01011@gmail.com>
2022-06-05 18:14:00 +0530

    RotationAndXOR

--- src/AdvancedDsa/String/RotationAndXOR.java
+++ src/AdvancedDsa/String/RotationAndXOR.java
@@ -2,32 +2,49 @@ package AdvancedDsa.String;
 
 public class RotationAndXOR {
     public static void main(String[] args) {
-        String A="1001";
-        String B="0011";
-        System.out.println(findCountOfXORs(A,B));
+        String A = "1001";
+        String B = "0011";
+        System.out.println(findCountOfXORs(A, B));
+        A = "111";
+        B = "111";
+        System.out.println(findCountOfXORs(A, B));
     }
 
     public static int findCountOfXORs(String A, String B) {
-        char[] arr=A.toCharArray();
-        int i=0,j=A.length()-1;
-
+        // By Simulation
+        int m = A.length();
+        int n = B.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            String bRot = rotate(B, i);
+//            int xor = findXOR(A, bRot);
+            boolean zeroXOR = isZeroXOR(A, bRot);
+//            if (xor == 0) {
+            if (zeroXOR) {
+                ans++;
+            }
+        }
+        return ans;
     }
 
-    public static String solve(String A, int B) {
-        char[]arr=A.toCharArray();
-        char[] temp=new char[A.length()];
-        B=B%A.length();
-        for (int i=A.length()-B,j=0;i<A.length();i++,j++){
-            temp[j]=arr[i];
-        }
+    private static boolean isZeroXOR(String a, String bRot) {
+        return a.equals(bRot);
+    }
 
-        for (int i=0,j=B;i<A.length()-B;i++,j++){
-            temp[j]=arr[i];
+    private static int findXOR(String a, String b) {
+        int l = a.length(); // or bRot.length(), as both are same length strings by constraint.
+        for (int i = 0; i < l; i++) {
+            int ia = a.charAt(i) - '0';
+            int ib = b.charAt(i) - '0';
+            if (ia != ib) {
+                return 1;
+            }
         }
+        return 0;
+    }
 
-//        for (char c:temp)
-//            System.out.println(c);
-        return new String(temp);
+    private static String rotate(String b, int i) {
+        return b.substring(i) + b.substring(0, i);
     }
 
 }
